5d3b74a7a442d36602c25c1765b35aa0a7c62a0a
Martin Maťovčík <martin.matovcik@stratox.cz>
2024-02-06 15:40:25 +0100

    revLineFeed to Zero update

--- TextPrint/cmr/CmrTextFileGenerator.cs
+++ TextPrint/cmr/CmrTextFileGenerator.cs
@@ -63,7 +63,8 @@ public class CmrTextFileGenerator
     private const int StartVariableRevLineFeedValue = 35;
     private const int EndVariableRevLineFeedValue = 25;
     private static readonly byte[] StartVariableRevLineFeedCommand = { 27, 106, StartVariableRevLineFeedValue };
-    private static readonly byte[] VariableLineFeedToZeroCommand = { 27, 106, 36 - StartVariableRevLineFeedValue };
+    private static readonly byte[] VariableLineFeedToZeroCommand = { 27, 106, 0 };
+    // private static readonly byte[] VariableLineFeedToZeroCommand = { 27, 106, 36 - StartVariableRevLineFeedValue };
 
     private const char EndOfLine = '\n';
     private const char Space = ' ';
