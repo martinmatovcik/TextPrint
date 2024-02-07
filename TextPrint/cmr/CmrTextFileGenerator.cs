0cb67195474ad14c6eda88159cdb1bfa37c6abfa
Martin Maťovčík <martin.matovcik@stratox.cz>
2024-02-07 11:36:04 +0100

    bez line feedu

--- TextPrint/cmr/CmrTextFileGenerator.cs
+++ TextPrint/cmr/CmrTextFileGenerator.cs
@@ -63,7 +63,7 @@ public class CmrTextFileGenerator
     private const int StartVariableRevLineFeedValue = 35;
     private const int EndVariableRevLineFeedValue = 25;
     private static readonly byte[] StartVariableRevLineFeedCommand = { 27, 106, StartVariableRevLineFeedValue };
-    private static readonly byte[] VariableLineFeedToZeroCommand = { 27, 74, 35 };
+    // private static readonly byte[] VariableLineFeedToZeroCommand = { 27, 74, 35 };
     // private static readonly byte[] VariableLineFeedToZeroCommand = { 27, 106, 36 - StartVariableRevLineFeedValue };
 
     private const char EndOfLine = '\n';
@@ -128,7 +128,7 @@ public class CmrTextFileGenerator
         var endFilePrinterCommands = JoinByteLists(new List<List<byte>>()
         {
             new(FormFeed),
-            new(VariableLineFeedToZeroCommand),
+            // new(VariableLineFeedToZeroCommand),
             new(variableLineFeedEndFile)
         });
 
