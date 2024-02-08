758b31bb53a265dfecdd0c350315ca43806ae912
Martin Maťovčík <martin.matovcik@stratox.cz>
2024-02-08 09:48:23 +0100

    line feed to zero - 2

--- TextPrint/cmr/CmrTextFileGenerator.cs
+++ TextPrint/cmr/CmrTextFileGenerator.cs
@@ -64,7 +64,7 @@ public class CmrTextFileGenerator
     private const int EndVariableRevLineFeedValue = 25;
     private static readonly byte[] StartVariableRevLineFeedCommand = { 27, 106, StartVariableRevLineFeedValue };
     // private static readonly byte[] VariableLineFeedToZeroCommand = { 27, 74, 35 };
-    // private static readonly byte[] VariableLineFeedToZeroCommand = { 27, 106, 36 - StartVariableRevLineFeedValue };
+    private static readonly byte[] VariableLineFeedToZeroCommand = { 27, 106, 2 };
 
     private const char EndOfLine = '\n';
     private const char Space = ' ';
@@ -215,7 +215,7 @@ public class CmrTextFileGenerator
             GenerateField20(formData) +
 
             //End
-            AddControlCode(endFilePrinterCommands);
+            AddControlCode(endFilePrinterCommands) + AddControlCode(VariableLineFeedToZeroCommand);
 
         return new MemoryStream(Encoding.GetBytes(cmrText));
     }
